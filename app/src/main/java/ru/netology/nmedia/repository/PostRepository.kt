d5219526098e54329e8cd26a1c5b60c8589cd9a6
Roman Ivanov <roman_ivanov85@internet.ru>
2023-04-15 18:00:46 +0300

    added enqueue

--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -2,11 +2,17 @@ package ru.netology.nmedia.repository
 
 import androidx.lifecycle.LiveData
 import ru.netology.nmedia.dto.Post
+import javax.security.auth.callback.Callback
 
 interface PostRepository {
-    fun getAll(): List<Post>
-    fun likeById(id: Long)
-    fun save(post: Post)
-    fun removeById(id: Long)
-    fun unlikeById(id: Long)
+    fun getAllAsync(postsCallback:PostsCallback<List<Post>>)
+    fun likeById(id: Long,postsCallback: PostsCallback<Post>)
+    fun save(post: Post, postsCallback: PostsCallback<Post>)
+    fun removeById(id: Long, postsCallback: PostsCallback<Unit>)
+    fun unlikeById(id: Long,postsCallback: PostsCallback<Post>)
+
+    interface PostsCallback<T> {
+        fun onSuccess(value: T)
+        fun onError(e: Exception)
+    }
 }
