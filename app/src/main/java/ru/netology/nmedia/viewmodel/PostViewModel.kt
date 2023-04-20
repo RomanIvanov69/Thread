5b51e024a77cae04484be73dece5462bcf0bec06
Roman Ivanov <roman_ivanov85@internet.ru>
2023-04-20 19:40:50 +0300

    added glide

--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -16,7 +16,8 @@ private val empty = Post(
     content = "",
     published = 0L,
     likedByMe = false,
-    likes = 0
+    likes = 0,
+    authorAvatar = null
 )
 
 class PostViewModel(application: Application) : AndroidViewModel(application) {
